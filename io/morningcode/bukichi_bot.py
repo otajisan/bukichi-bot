169f3c6ee51b77ff0de7068a3ac11cfa16881d39
otajisan <taji.taji.otajisan@gmail.com>
2023-04-21 15:19:33 +0900

    :+1: fix character display

--- io/morningcode/bukichi_bot.py
+++ io/morningcode/bukichi_bot.py
@@ -1,4 +1,6 @@
 import os
+import textwrap
+
 import discord
 
 from components.image_creator import ImageCreator
@@ -8,6 +10,8 @@ DISCORD_APP_TOKEN = os.getenv('DISCORD_APP_TOKEN')
 
 class BukichiBotClient(discord.Client):
 
+    DEBUG = False
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
@@ -15,6 +19,29 @@ class BukichiBotClient(discord.Client):
         if message.author.bot:
             return
         print(f'Message from {message.author}: {message.content}')
+        if self.DEBUG:
+            await self.debug_image(message)
+
+    async def debug_image(self, message):
+        member = message.author
+        guild = member.guild
+
+        image_creator = ImageCreator()
+
+        names = [
+            'foo',
+            'テスト',
+            'てすと',
+            'テスト太郎',
+            'Discordマン',
+        ]
+        for name in names:
+            image = image_creator.greeting_by_name(name)
+            if image != '':
+                await guild.system_channel.send(file=discord.File(image))
+            else:
+                plain_greeting_message = await self.get_greeting_message(name)
+                await guild.system_channel.send(plain_greeting_message)
 
     async def on_member_join(self, member):
         print(f'New member joined. {member}')
@@ -23,10 +50,20 @@ class BukichiBotClient(discord.Client):
         if guild.system_channel is not None:
             # to_send = f'Welcome {member.mention} to {guild.name}!'
             # await guild.system_channel.send(to_send)
+            name = member.name
             image_creator = ImageCreator()
-            greeting_message = 'さん、はじめましてでし！'
-            image = image_creator.from_text(member.name, greeting_message)
-            await guild.system_channel.send(file=discord.File(image))
+            image = image_creator.greeting_by_name(name)
+            if image != '':
+                await guild.system_channel.send(file=discord.File(image))
+            else:
+                plain_greeting_message = await self.get_greeting_message(name)
+                await guild.system_channel.send(plain_greeting_message)
+
+    async def get_greeting_message(self, name):
+        return textwrap.dedent(f'''
+                {name} さん、はじめましてでし！
+                まずは #ルール をよんでね。
+                ''')
 
 
 intents = discord.Intents.default()
--- io/morningcode/components/image_creator.py
+++ io/morningcode/components/image_creator.py
@@ -1,31 +1,63 @@
 import os
+import re
 
 import PIL.Image
 import PIL.ImageDraw
 import PIL.ImageFont
+import regex
 
 
 class ImageCreator:
     ENGLISH_FONT_PATH = os.getenv('ENGLISH_FONT_PATH')
     JAPANESE_FONT_PATH = os.getenv('JAPANESE_FONT_PATH')
 
-    def from_text(self, name, text):
+    REGEX_HIRAGANA_KATAKANA = regex.compile(r'[\p{Script=Hiragana}\p{Script=Katakana}ーa-z]+')
+    REGEX_ALPHABETS = re.compile('[a-zA-Z0-9]+')
+
+    def greeting_by_name(self, name):
         image_file_name = 'image.png'
         fontsize = 36
 
-        canvasSize = (800, 150)
-        backgroundRGB = (0, 0, 0)
-        textRGB = (255, 255, 255)
+        canvas_size = (800, 180)
+        background_color = (0, 0, 0)
+        text_color = (255, 255, 255)
+        strong_text_color = (255, 165, 0)
 
-        img = PIL.Image.new('RGB', canvasSize, backgroundRGB)
+        img = PIL.Image.new('RGB', canvas_size, background_color)
         draw = PIL.ImageDraw.Draw(img)
 
         font_japanese = PIL.ImageFont.truetype(self.JAPANESE_FONT_PATH, fontsize)
         font_english = PIL.ImageFont.truetype(self.ENGLISH_FONT_PATH, fontsize)
 
+        # x
+        start_x = 20
+        delta = 18
+        pos_x = start_x
+        # y
+        line_1_y = 20
+        line_space = 50
 
-        draw.text((20, 20), name, fill=textRGB, font=font_english)
-        draw.text((20, 70), text, fill=textRGB, font=font_japanese)
+        for idx, c in enumerate(name):
+            if self.REGEX_ALPHABETS.fullmatch(c):
+                name_font = font_english
+                delta = 18
+                line_1_y = 10
+            elif self.REGEX_HIRAGANA_KATAKANA.fullmatch(c):
+                name_font = font_japanese
+                delta = 28
+                line_1_y = 20
+            else:
+                print(f'invalid character found. name: {name} c: {c}')
+                return ''
+
+            # print(f'c: {c} name_font: {name_font}')
+            pos_x = start_x + (delta * idx)
+            draw.text((pos_x, line_1_y), c, fill=strong_text_color, font=name_font)
+        draw.text((pos_x + delta + 10, 20), 'さん、', fill=text_color, font=font_japanese)
+        draw.text((start_x, line_1_y + line_space), 'はじめましてでし！', fill=text_color,
+                  font=font_japanese)
+        draw.text((start_x, line_1_y + (line_space * 2)), 'まずはルールをよんでね。', fill=text_color,
+                  font=font_japanese)
 
         img.save(image_file_name)
 
